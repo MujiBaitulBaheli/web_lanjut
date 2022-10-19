1f9f5a7264c4b3cadbd240d5f38351556cca6bb3
MujiBaitulBaheli <49540760+MujiBaitulBaheli@users.noreply.github.com>
2022-10-19 14:59:59 +0800

    Add files via upload

---
+++ storage/framework/views/127e7f182e4de7c3c0bc0f32bd9fbc0a9322f516.php
@@ -0,0 +1,1 @@
+<?php /**PATH C:\Users\ASUS\web_lanjut\resources\views/home.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c02e5eed1aa265703e8b6b3940a526271d92d2af.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    pt jaya makmur
+</body>
+</html>
+<?php /**PATH C:\Users\ASUS\web_lanjut\resources\views/pt.blade.php ENDPATH**/ ?>
